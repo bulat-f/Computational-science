12f6e41b848d9ff2e932ec59ded10df3f25961c2
Fatbulat <fatbulat5@gmail.com>
2015-03-11 19:41:04 +0300

    Implement Taylor Series for array of nodes

--- taylor.py
+++ taylor.py
@@ -1,4 +1,5 @@
 import math
+from array import array
 
 
 class erf:
@@ -14,13 +15,19 @@ class erf:
             q = self.quotient(x, n)
         return (2 / math.sqrt(math.pi)) * erf
 
+    def arr_taylor(self, x, eps=1e-6):
+        erf = []
+        for i in range(0, len(x)):
+            erf.append(self.taylor(x[i], eps))
+        return erf
+
     def quotient(self, x, n):
         return (-x * x * (2 * n + 1)) / ((n + 1) * (2 * n + 3))
 
 
 def main():
     f = erf()
-    print(f.taylor(0.8))
+    print(f.arr_taylor([0.2, 0.6, 0.8]))
 
 if __name__ == '__main__':
     main()
