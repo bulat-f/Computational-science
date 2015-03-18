7b304b6ab07b9226241ce9083522303c234e7038
Fatbulat <fatbulat5@gmail.com>
2015-03-18 16:31:47 +0300

    Implement Lagrange polynomial

--- taylor.py
+++ taylor.py
@@ -2,7 +2,7 @@ import math
 from array import array
 
 
-class erf:
+class Erf:
     def taylor(self, x, eps=1e-6):
         a = x
         erf = x
@@ -21,13 +21,31 @@ class erf:
             erf.append(self.taylor(x[i], eps))
         return erf
 
+    def lagrange(self, nodes, values, x):
+        Ln = 0
+        for i in range(0, len(nodes)):
+            F = 1
+            for j in range(0, len(nodes)):
+                if i != j:
+                    F *= (x - nodes[j]) / (nodes[i] - nodes[j])
+            Ln += values[i] * F
+        return Ln
+
     def quotient(self, x, n):
         return (-x * x * (2 * n + 1)) / ((n + 1) * (2 * n + 3))
 
+def create_nodes(a, b, n):
+    nodes = []
+    h = (b - a) / (n)
+    for i in range(n + 1):
+        nodes.append(a + i * h)
+    return nodes
 
 def main():
-    f = erf()
-    print(f.arr_taylor([0.2, 0.6, 0.8]))
+    f = Erf()
+    nodes = create_nodes(0, 2, 10)
+    values = f.arr_taylor(nodes)
+    print(f.arr_taylor(nodes))
 
 if __name__ == '__main__':
     main()
