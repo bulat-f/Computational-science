3234bea1fbd4085e3ab7f18d1e69d65f65c864e4
Fatbulat <fatbulat5@gmail.com>
2015-04-15 16:12:19 +0300

    Implement method for computing in nodes array

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def create_nodes(a, b, n):
         nodes.append(a + i * h)
     return nodes
 
+def method_for_array(arr, method):
+    result = []
+    for i in range(0, len(arr)):
+        result.append(method(arr[i]))
+    return result
+
 def main():
     f = erf.Erf()
-    # nodes = create_nodes(0, 2, 10)
-    # values = f.taylor(nodes)
-    print(f.integral(0.8, 'simpson'))
-    print(f.taylor(0.8))
+    nodes = create_nodes(0, 2, 10)
+    values = method_for_array(nodes, f.taylor)
+    print(f.lagrange(nodes, values, 0.25))
+    print(f.taylor(0.25))
 
 if __name__ == '__main__':
     main()
