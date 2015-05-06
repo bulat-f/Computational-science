e79d25836fb9f9eb89b44bef0a4ea1c2342318c2
Fatbulat <fatbulat5@gmail.com>
2015-05-06 10:22:17 +0300

    Add method for generate table for Lagrange interpolation polinom errors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import report
 def main():
     f = erf.Erf()
     report.taylor(0, 2, 10)
+    report.lagrange(0, 2)
     print(f.taylor(1.25))
     print(f.inverse(0.922900105827684))
     # print(compare.chebyshev_max_error(0, 2, f.lagrange, f.taylor, 10))
--- report.py
+++ report.py
@@ -12,6 +12,9 @@ def write_tabs(a, digits, out):
         out.write('$' + rounding % x + '$' + ('&' if counter < n else '\\\\\n'))
     return
 
+def tex_siquence(arr):
+    return str(arr[0]) + ', ' + str(arr[1]) + '\\dots' + str(arr[len(arr) - 1])
+
 # formulagte report files
 def taylor(a, b, n):
     out = open('./tex/taylor.tex', 'w')
@@ -22,5 +25,30 @@ def taylor(a, b, n):
     values = nodes.method_for_array(taylor_nodes, f.taylor)
     out.write('\hline\n')
     write_tabs(taylor_nodes, 2, out)
+    out.write('\hline\n')
     write_tabs(values, 6, out)
+    out.write('\\end{tabular}')
+
+def lagrange(a, b):
+    out = open('./tex/lagrange.tex', 'w')
+    f = erf.Erf()
+    nodes_count = []
+    chebyshev = []
+    equidistant = []
+    for i in range(8):
+        nodes_count.append(8 + 2*i)
+    for count in nodes_count:
+        chebyshev.append(compare.chebyshev_max_error(a, b, f.lagrange, f.taylor, count))
+        equidistant.append(compare.equidistant_max_error(a, b, f.lagrange, f.taylor, count))
+    out.write('Протабулируем $L_n(x)$ на отрезке [' + str(a) + ', ' + str(b) + '], где $n =' + tex_siquence(nodes_count) + '$ и вычилим погрешность в равностоящих $2n$ узлах\\\\\n')
+    out.write('\\begin{tabular}{' + 'r|' + 'c'*len(nodes_count) + '}\n')
+    out.write('\hline\n')
+    out.write('Кол-во узлов&')
+    write_tabs(nodes_count, 0, out)
+    out.write('\hline\n')
+
+    out.write('равн. узлы&')
+    write_tabs(equidistant, 8, out)
+    out.write('Чеб. узлы&')
+    write_tabs(chebyshev, 8, out)
     out.write('\\end{tabular}')
